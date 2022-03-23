8ecf8f619d51bed2196eeb4b5eebef416e068a8e
anacarpere <86481653+anacarpere@users.noreply.github.com>
2022-03-22 22:42:23 -0300

    Inclusão do exercício

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material/material.module';
+import { ConfirmDialogComponent } from './components/shared/confirm-dialog/confirm-dialog.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ConfirmDialogComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/services/senha.service.ts
@@ -0,0 +1,16 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class SenhaService {
+  passwordConfirm(password: string, passwordConfirm: string): boolean{
+    if(password !== passwordConfirm){
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  constructor() { }
+}
